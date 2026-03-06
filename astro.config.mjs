c787f4ea51bd233b15b92980068183a13dc09514
Nahuel <nahuelgenari@gmail.com>
2026-03-06 15:34:19 -0300

    og image added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Agregamos la URL base (obligatorio para SEO y Sitemaps)
+  site: 'https://calendarium.tech',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [
+    react(), 
+    // 2. La integración del sitemap ya estaba, pero ahora tiene la URL arriba para funcionar
+    sitemap()
+  ]
 });
