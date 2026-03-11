f5bf67dbdec687b45d8892936609210ff47aae7f
Nahuel <nahuelgenari@gmail.com>
2026-03-11 00:15:17 -0300

    fix: correct fix-redirects script syntax

--- fix-redirects.mjs
+++ fix-redirects.mjs
@@ -1,4 +1,3 @@
-import { writeFileSync } from "fs";
-writeFileSync("dist/_redirects", "/* /.netlify/functions/ssr 200
-");
-console.log("_redirects fixed!");
+import { writeFileSync } from 'fs';
+writeFileSync('dist/_redirects', '/* /.netlify/functions/ssr 200\n');
+console.log('_redirects fixed!');
