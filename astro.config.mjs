59d456d39b43ee29eb08e4a0a7b276acd21cde7d
Nahuel <nahuelgenari@gmail.com>
2026-03-17 11:54:18 -0300

    fixed astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'static',
   site: 'https://calendarium.tech',
-  // Borramos output: 'hybrid' -> Ahora es el comportamiento base
-  
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react(), sitemap()],
-  adapter: node({
-    mode: 'standalone'
-  })
+  integrations: [react(), sitemap()]
 });
